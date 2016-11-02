9172fdf1bdd7022935465bc15b15b1b441befb50
tfchen <tfchen5211@foxmail.com>
2016-11-02 16:26:47 +0800

    添加观察者模式测试

---
+++ src/test/java/com/hangzhou/tfchen/observer/ObserverTest.java
@@ -0,0 +1,19 @@
+package com.hangzhou.tfchen.observer;
+
+/**
+ * @author chentf(水言Dade)
+ * @e-mail tfchen5211@foxmail.com
+ * @date 2016/11/2 16:12
+ * @描述：观察者模式测试
+ * @注意事项：
+ */
+
+public class ObserverTest {
+
+    public ObserverTest() {
+    }
+
+    public void observerTest() {
+
+    }
+}
